0494a119b33f095b6bbc86c1b1eee847a12dc8c4
krxlxrdx3926x <144669275+krxlxrdx3926x@users.noreply.github.com>
2025-11-12 17:45:15 +0100

    Add files via upload

---
+++ logowanie.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+
+$errors = [
+  'login' => $_SESSION['login_error'] ?? '',
+  'register' => $_SESSION['register_error'] ?? ''
+
+];
+
+$activeForm = $_SESSION['active_form'] ?? 'login';
+
+session_unset();
+
+function showError($error) {
+  return !empty($error) ? "<p class='error-massage>$error</p>" : '';
+}
+
+function isActiveForm($formName, $activeForm) {
+  return $formName === $activeForm ? 'active' : '';
+}
+
+?>
+
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta
+      name="viewport"
+      content=""
+      width="device-width,"
+      initial-scale="1.0"
+    />
+    <link rel="stylesheet" href="css.css" />
+  </head>
+  <body>
+    <div class="container">
+      <div class="form-box <?= isActiveForm('login', $activeForm); ?> " id="login-form">
+        <form action="login_register.php" method="post">
+          <h1>Login</h1>
+          <?= showError($errors['login']); ?>
+          <input type="email" name="email" placeholder="Email" />
+          <input type="password" name="password" placeholder="Hasło" />
+          <button type="submit" name="login">Login</button>
+
+          <p>
+            Nie masz konta?
+            <a href="#" onclick="showForm('register-form')"> Rejestracja</a>
+          </p>
+        </form>
+      </div>
+
+      <div class="form-box <?= isActiveForm('register', $activeForm); ?>" id="register-form">
+        <form action="login_register.php" method="post">
+          <h1>Rejestracja</h1>
+          <?= showError($errors['register']); ?>
+
+          <input type="text" name="name" placeholder="Imie" />
+          <input type="email" name="email" placeholder="Email" />
+          <input type="password" name="password" placeholder="Hasło" />
+          <button type="submit" name="register">Zarejestruj się</button>
+
+          <p>
+            Masz konto?
+            <a href="#" onclick="showForm('login-form')"> Zaloguj się</a>
+          </p>
+        </form>
+      </div>
+    </div>
+    <script src="script.js"></script>
+  </body>
+</html>
---
+++ script.js
@@ -0,0 +1,6 @@
+function showForm(formId) {
+  document.querySelectorAll(".form-box").forEach((form) => {
+    return form.classList.remove("active");
+  });
+  document.getElementById(formId).classList.add("active");
+}
